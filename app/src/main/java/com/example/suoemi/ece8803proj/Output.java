ca0964826a04db61485674ae1fb80f8e7816b3e1
SKoroye <suoemikoroye@gmail.com>
2017-04-17 19:31:48 -0400

    7:31

--- app/src/main/java/com/example/suoemi/ece8803proj/Output.java
+++ app/src/main/java/com/example/suoemi/ece8803proj/Output.java
@@ -18,14 +18,19 @@ import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.joptimizer.optimizers.LPOptimizationRequest;
 import com.joptimizer.optimizers.LPPrimalDualMethod;
 import com.joptimizer.optimizers.OptimizationResponse;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.fail;
@@ -45,6 +50,11 @@ public class Output extends AppCompatActivity {
     private int u1;
     private int returnCode;
     public double[] sol;
+    private double[] c;
+    private double[] B;
+    private double[] ub;
+    private Map<String, Object> usr_sell;
+    private Map<String, Object> usr_buy;
     public double outp;
     public double outa;
 
@@ -138,20 +148,77 @@ public class Output extends AppCompatActivity {
         LPOptimizationRequest or = new LPOptimizationRequest();
         List<LoginData> loginDatasell = db.getAllSellLog();
 
-        for(LoginData loginData : loginDatasell)
-        {
-            if(loginData.getCheck() == 1)
-            {
-                this.c1 = loginData.getePrice();
-                this.u1 = loginData.geteBid();
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                usr_sell = (Map<String,Object>) dataSnapshot.getValue();
             }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+                // ...
+            }
+        };
+
+        databaseref.child("sellers").addValueEventListener(postListener);
+
+
+        ValueEventListener postListener2 = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                usr_buy = (Map<String, Object>) dataSnapshot.getValue();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+                // ...
+            }
+        };
+
+        databaseref.child("ev drivers").addValueEventListener(postListener2);
+
+        ArrayList<Double> bidamt = new ArrayList<>();
+        ArrayList<Double> bidpr = new ArrayList<>();
+        ArrayList<Double> evreq = new ArrayList<>();
+
+        for(Map.Entry<String, Object> entry : usr_sell.entrySet()){
+            Map singlesell = (Map) entry.getValue();
+            bidamt.add((Double) singlesell.get("bid amount"));
+            bidpr.add((Double) singlesell.get("bid price"));
+        }
+        for(Map.Entry<String, Object> entry : usr_buy.entrySet()){
+            Map singlebuy = (Map) entry.getValue();
+            evreq.add((double) singlebuy.get("ev req"));
         }
 
-        double[] c = new double[] {c1, 40, 20};
+        c = new double[bidpr.size()];
+        for (int i = 0; i<c.length; i++)
+        {
+            c[i] = bidpr.get(i).doubleValue();
+            System.out.print("Price" + c);
+        }
+
         double[][] A = new double[][] {{1, 1, 1}};
-        double[] B = new double[] {110};
+
+        B = new double[evreq.size()];
+        for (int i = 0; i<B.length; i++)
+        {
+            B[i] = evreq.get(i).doubleValue();
+        }
+
         double[] lb = new double[] {0, 0, 0};
-        double[] ub = new double[] {u1, 100, 50};
+
+        ub = new double[bidamt.size()];
+        for (int i = 0; i<ub.length; i++)
+        {
+            ub[i] = bidamt.get(i).doubleValue();
+        }
 
         or.setC(c);
         or.setA(A);
